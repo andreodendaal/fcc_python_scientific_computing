7ecd6f5cbe70553a2403fce79177cf72f30ae7ca
andreodendaal <aodendaal.direct@gmail.com>
2022-03-18 16:49:27 -0700

    andreodendaal commit

--- budget_app/budget.py
+++ budget_app/budget.py
@@ -9,27 +9,28 @@ class Category:
         print('amount: ' + str(self.amount))    
 		
 
-    def deposit(self, depositAmount, description):
-        self.descriptiom = description
+    def deposit(self, depositAmount, description=""):
+        self.description = description
         self.depositAmount = depositAmount
-        self.ledger_object = {"amount": self.depositAmount, "description": self.description}
-        self.ledger.append(self.ledger_object)
+        ledger_object = {"amount": self.depositAmount, "description": self.description}
+        self.ledger.append(ledger_object)
         self.amount = self.amount + self.depositAmount         
         
         print('deposit amount: ' + str(self.amount))
         print('deposit ledger: ' + str(self.ledger))
 
-    def withdraw(self, withdrawAmount, description):
+    def withdraw(self, withdrawAmount, description=""):
         self.withdrawAmount = withdrawAmount
         self.description = description
-        
+
         if self.withdrawAmount > 0:
-            self.withdrawAmount = self.withdrawAmount * -1
+            withdrawAmount_neg = self.withdrawAmount * -1
         
-        if self.amount + self.withdrawAmount >= 0:
-            print('withdraw amount: ' + str(self.withdrawAmount))
-            self.amount = self.amount + self.withdrawAmount
-            self.ledger_object = {"amount": self.amount, "description": self.descriptiom}
+        if self.amount + withdrawAmount_neg >= 0:
+            print('withdraw amount: ' + str(withdrawAmount_neg))
+            self.amount = self.amount + withdrawAmount_neg
+            ledger_object = {"amount": withdrawAmount_neg, "description": self.description}
+            self.ledger.append(ledger_object)
             
             print('withdraw ledger: ' + str(self.ledger))
             print('new Amount: ' + str(self.amount))
@@ -38,12 +39,15 @@ class Category:
             print("No withdraw: " + str(self.withdrawAmount) + ' > ' + str(self.amount))
             return False
 
+    def get_balance(self):
+        return self.amount
+
 def create_spend_chart(categories):
     return('100|')
 
 
-food = Category("Food")
-food.deposit(10, "initial deposit")
-food.deposit(2, "2nd deposit")
-food.withdraw(3 , "1st withdraw")
-food.withdraw(20, "ineligible withdraw")
+# food = Category("Food")
+# food.deposit(10, "initial deposit")
+# food.deposit(2, "2nd deposit")
+# food.withdraw(3 , "1st withdraw")
+# food.withdraw(20, "ineligible withdraw")
