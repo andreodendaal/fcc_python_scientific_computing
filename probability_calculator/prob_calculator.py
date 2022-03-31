b898f926bac61f87e742ddb1d065521a71dbcd18
andreodendaal <aodendaal.direct@gmail.com>
2022-03-30 20:42:48 -0700

    Interim commit, not working

--- probability_calculator/prob_calculator.py
+++ probability_calculator/prob_calculator.py
@@ -29,10 +29,30 @@ class Hat:
         return random_draw 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+    # For example, let's say that you want to determine the probability of getting at least 2 red balls and 1 green ball 
+    # when you draw 5 balls from a hat containing 6 black, 4 red, and 3 green. 
 
-hat = Hat(red=5,blue=2)
-print(hat.draw(2))
+    # To do this, we perform N experiments, count how many times M we get at least 2 red balls and 1 green ball, and estimate the probability as M/N. 
+    # Each experiment consists of starting with a hat containing the specified balls, drawing a number of balls, 
+    # and checking if we got the balls we were attempting to draw.
+    expected_match = 0
+    
+        
+    draw_hat = hat.contents
+        # do draw
+    result = hat.draw(num_balls_drawn)
+        # compare draw
+
+        # if draw equals expected_ balls add to counter
+    expected_match += 1
+    
+    #Calculate the probability
+    probability = expected_match/num_experiments
+
+    return probability
+
+# hat = Hat(red=5,blue=2)
+# print(hat.draw(2))
 
 # hat = Hat(red=3,blue=2)
 # print(hat.contents) 
